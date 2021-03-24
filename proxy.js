337bc9e51809bbe6ebb5ff0bc544da81c8ef6cfc
Doug <douganderson444@gmail.com>
2021-03-24 11:15:56 -0400

    add hyperspace logic

--- proxy.js
+++ proxy.js
@@ -4,15 +4,47 @@ const send = require('send')
 const path = require('path')
 const DEFAULT_PORT = 4977 // HYPR on a cellphone keypad
 
+const HyperspaceClient = require('@hyperspace/client')
+
 const INDEX_HTML_LOCATION = path.join(__dirname, 'index.html')
 
-module.exports = (network = {}, port = DEFAULT_PORT) => {
+module.exports = async ({ network = {}, port = DEFAULT_PORT }) => {
+  // If there is a Hyperspace daemon running, use that for the network
+  let opts = {}
+  if (!network) {
+    // check for a possible hyperspace presence
+    try {
+      // wait until the server is ready
+      const spacePromise = HyperspaceClient.serverReady({}) // should timeout, because this will wait forever
+
+      const timePromise = new Promise((resolve, reject) => {
+        setTimeout(resolve, 1000, 'one') // timeout after a second, that's 10 trys
+      })
+
+      await Promise.race([timePromise, spacePromise])
+
+      console.log((new Date()).toISOString(), ' - Hyperspace ready \n')
+
+      const client = new HyperspaceClient()
+
+      // wait for client to be ready
+      await client.ready()
+      opts = client && client.network && typeof client.network === 'object' ? { network: client.network } : {}
+    } catch (error) {
+      console.error('no hyperspace client', error)
+      opts = {}
+    }
+  } else {
+    opts = { network }
+  }
+  console.log({ opts })
+
   const server = http.createServer(function onRequest (req, res) {
     send(req, INDEX_HTML_LOCATION)
       .pipe(res)
   })
 
-  const wsServer = new HyperswarmServer() // { network }
+  const wsServer = new HyperswarmServer(opts) // { network }
 
   wsServer.listenOnServer(server)
 
